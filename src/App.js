0bde36994e364344ccf9c8680f0dd9467042a619
jakdev047 <shuvo047niter@gmail.com>
2020-04-08 12:09:18 +0600

    to add component file of Todo.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import Todos from './components/Todos';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <Container>
+        <Todos />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todos = () => {
+  return (
+    <>
+      <h2 className="display-4 my-3 text-center">Todos</h2>
+    </>
+  );
+};
+
+export default Todos;
